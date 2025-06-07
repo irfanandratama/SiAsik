738c1de09659434883459cfcd1ced126ed41160a
Irfan Hanandra <Ecomindo3.Vendor@interport.co.id>
2025-06-08 05:11:13 +0700

    Make seeder

---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,516 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('rooms')->insert([
+            'name' => 'Lobby PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Bank BTN',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Istirahat PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Back Office PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Meubelair dan Perangkat Komputer PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Pengunjung (G Ke 1)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Smoking (Depan Bawah Tangga Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Parkir Mobil Depan PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Depan Bawah Tangga',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Arsip',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Arsip I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Arsip II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Arsip III',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Arsip',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Jaksa',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Polisi',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Lobby Tahanan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Tahanan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tahanan Anak',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tahanan Laki - laki',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Tahanan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Kesehatan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Bekas Server',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Entrance Tahanan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Parkir Mobil Belakang',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Entrance',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Belakang (Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tunggu Supir Belakang',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Perawatan Taman',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Entrance PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'PTSP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Musholla (Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Pria (Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Wanita (Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Disabilitas (Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Janitor (Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Darurat Depan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Panel',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Depan (Lantai Ground)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Ortala',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Perdata',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hukum',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Pidana',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Calon Hakim',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Dharmayukti',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Perpustakaan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Kepala Bagian Umum',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang TU dan Keuangan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Kiri (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Pria dan Wanita (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Janitor/Disabilitas (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Kanan (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Area Merokok (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Belakang (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Pantry (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Pria dan Wanita Belakang (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Juru Sita',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang PTIP',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Media Center',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Arsip (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Musholla (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Rapat (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Belakang (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Lobby Lantai 3',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Depan (Lantai 3)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Depan (Lantai 1)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Tirta I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Kartika II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Utama',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Sari I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tunggu Depan Sidang Utama',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Lobby Lantai 1',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tunggu Depan Sidang Tirta',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Entrance Lantai 1',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Kiri Persidangan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Turun',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Cakra I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Cakra II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Bapas dan Pekerja Sosial',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tunggu Ramah Anak',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Ramah Anak',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Laktasi',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Panel (Lantai 1)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Pria dan Wanita (Lantai 1)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Disabilitas (Lantai 1)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Kanan Persidangan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tunggu Hakim',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Belakang (Lantai 1)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Mediasi',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Diversi',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Sari II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Candra I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sidang Candra II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Lobby Lounge (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Rapat (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Kanan Sekretaris (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Pria dan Wanita Depan (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Janitor Depan (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Pantry (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Depan (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Panitera',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Tangga Darurat Belakang (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Wakil Ketua',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Sekretaris',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Ajudan',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Lobby Tengah (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Kiri Ketua (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Toilet Pria dan Wanita Belakang (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Janitor (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Musholla (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Area Merokok (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Selasar Belakang (Lantai 2)',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Ketua',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Tamu Ketua',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim VI',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Panitera Pengganti I',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim III',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim IV',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim V',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim VIII',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Hakim VII',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Panitera Pengganti II',
+        ]);
+
+        DB::table('rooms')->insert([
+            'name' => 'Ruang Panitera Pengganti III',
+        ]);
+
+    }
+}
