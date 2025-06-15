ecccb3d43c21c0fed1809677cd7dad8e580330e4
Irfan Hanandra <Ecomindo3.Vendor@interport.co.id>
2025-06-16 00:32:02 +0700

    Email and database notification with change of logo

---
+++ resources/views/emails/newreport.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laporan Baru Telah Masuk Melalui Aplikasi SiAsik</title>
+</head>
+<body>
+    <h2>Halo, {{ $data['name'] }}!</h2>
+    <p>{{ $data['message'] }}</p>
+    <p>Terima kasih!</p>
+</body>
+</html>
